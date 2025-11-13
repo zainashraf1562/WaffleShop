4249c1a264f10b97733998733a731f9f0bf80bf0
Zain Ashraf <zainashraf1562@gmail.com>
2025-11-13 15:31:21 -0500

    Product class finished

--- src/main/java/com/pluralsight/waffleShop/Products/Product.java
+++ src/main/java/com/pluralsight/waffleShop/Products/Product.java
@@ -1,4 +1,15 @@
 package com.pluralsight.waffleShop.Products;
 
 public abstract class Product {
+    private String name;
+
+    Product(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public abstract double getPrice();
 }
