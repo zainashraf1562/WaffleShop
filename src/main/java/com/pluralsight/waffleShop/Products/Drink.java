206bfdee551a078056299bf81d36ffef94bb6a0b
zainashraf1562 <zainashraf1562@gmail.com>
2025-11-13 21:06:12 -0500

    finsihed products

--- src/main/java/com/pluralsight/waffleShop/Products/Drink.java
+++ src/main/java/com/pluralsight/waffleShop/Products/Drink.java
@@ -1,4 +1,36 @@
 package com.pluralsight.waffleShop.Products;
 
 public class Drink extends Product{
+    private String size;
+
+    public Drink(String name, String size) {
+        super(name);
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    @Override
+    public double getPrice() {
+        double price = 0;
+        if (size.equalsIgnoreCase("small")){
+            price = 5;
+        } else if (size.equalsIgnoreCase("medium")) {
+            price = 7;
+        } else if (size.equalsIgnoreCase("large")) {
+            price = 10;
+        }
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return " (" + size + ") " + getName() + " shake: $" + getPrice();
+    }
 }
--- src/main/java/com/pluralsight/waffleShop/Products/Icecream.java
+++ src/main/java/com/pluralsight/waffleShop/Products/Icecream.java
@@ -1,4 +1,19 @@
 package com.pluralsight.waffleShop.Products;
 
 public class Icecream extends Product{
+    public Icecream(String name){
+        super(name);
+    }
+
+    @Override
+    public double getPrice() {
+        return 2.0;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + " IceCream: $" + getPrice();
+    }
+
+
 }
