07a3871d57e93b4492f4f1dc461f9e119d770f00
zainashraf1562 <zainashraf1562@gmail.com>
2025-11-14 02:10:15 -0500

    fixed error

--- src/main/java/com/pluralsight/waffleShop/OrderData/ReceiptDataManager.java
+++ src/main/java/com/pluralsight/waffleShop/OrderData/ReceiptDataManager.java
@@ -9,35 +9,82 @@ import java.time.format.DateTimeFormatter;
 
 public class ReceiptDataManager {
     public void saveReceipt(Cart cart){
-        LocalDateTime timeNow = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-hhmmss");
-        String fileName = "receipts/" + timeNow.format(formatter) + ".txt";
+        try {
+        if (cart == null) {
+            System.out.println("Order was null. Cannot save receipt.");
+            return;
+        }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        String time = now.format(formatter);
 
-            writer.write("======== DELI-BROS ========\n");
-            writer.write("  THANK YOU FOR SHOPPING!\n");
+        String fileName = "receipt." + time + ".txt";
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+
+            writer.write("======== WAFFLE SHOP ========\n");
+            writer.write("  THANK YOU FOR SHOPPING! \n");
             writer.write("===========================\n");
 
-            DateTimeFormatter receiptFormatter = DateTimeFormatter.ofPattern("MMM dd, yyyy HH:mm:ss");
-            writer.write("DATE: " + timeNow.format(receiptFormatter) + "\n\n");
+            writer.write("DATE: " + time + "\n\n");
 
             writer.write("ITEMS:\n");
             writer.write("---------------------------\n");
 
-            for (Product product : cart.getMyCart()) {
-                writer.write(product.toString() + "\n");
-            }
+        for (Product product : cart.getMyCart()) {
+            writer.write(product.getName() + " | " + "$" + product.getPrice());
+            writer.newLine();
+        }
 
             writer.write("\n---------------------------\n");
             writer.write("TOTAL: $" + String.format("%.2f", cart.getTotalCost()) + "\n");
             writer.write("===========================\n");
             writer.write("        COME AGAIN!        \n");
             writer.write("===========================\n");
+            writer.close();
+            System.out.println("Receipt Saved!");
 
-        } catch (Exception ex) {
-            System.err.println("Error Saving File");
-        }
+    }catch (Exception ex) {
+        System.err.println("Error Saving Receipt");
+    }
+
+//        if (cart == null) {
+//            System.out.println("Empty Cart!");
+//            return;
+//        }
+//
+//        LocalDateTime now = LocalDateTime.now();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-hhmmss");
+//        String time = now.format(formatter);
+//
+//        String fileName = "receipt." + now + ".txt";
+//
+//        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+//
+//            writer.write("======== WAFFLE SHOP ========\n");
+//            writer.write("  THANK YOU FOR SHOPPING!\n");
+//            writer.write("===========================\n");
+//
+//            writer.write("DATE: " + time + "\n\n");
+//
+//            writer.write("ITEMS:\n");
+//            writer.write("---------------------------\n");
+//
+//            for (Product product : cart.getMyCart()) {
+//                writer.write(product.toString() + "\n");
+//            }
+//
+//            writer.write("\n---------------------------\n");
+//            writer.write("TOTAL: $" + String.format("%.2f", cart.getTotalCost()) + "\n");
+//            writer.write("===========================\n");
+//            writer.write("        COME AGAIN!        \n");
+//            writer.write("===========================\n");
+//            writer.close();
+//            System.out.println("Receipt Saved!");
+//
+//        } catch (Exception ex) {
+//            System.err.println("Error Saving Receipt");
+//        }
     }
 
 }
