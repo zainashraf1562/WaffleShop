de1daf84003b7672ebc9a47753b6ab6ad45dfb4f
zainashraf1562 <zainashraf1562@gmail.com>
2025-11-13 19:38:04 -0500

    finished toppings class

--- src/main/java/com/pluralsight/waffleShop/Toppings/Topping.java
+++ src/main/java/com/pluralsight/waffleShop/Toppings/Topping.java
@@ -1,4 +1,34 @@
 package com.pluralsight.waffleShop.Toppings;
 
 public abstract class Topping {
+    private String name;
+    private String size;
+    private boolean isExtra;
+
+    public Topping(String name, boolean extra){
+        this.name = name;
+        isExtra = extra;
+    }
+
+    public Topping(String name, String size, boolean extra){
+        this.name = name;
+        this.size = size;
+        isExtra = extra;
+    }
+
+    public double getPrice(){
+        return 0.0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public boolean isExtra() {
+        return isExtra;
+    }
 }
